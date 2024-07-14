b5b2f491a3610a4503efb882f4b562ac7a550db9
tommaho <tom@tommaho.com>
2024-07-13 21:25:09 -0400

    Frame up html, js, css for refinement.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //Odin Project Calculator
+//make a calculator that supports add subtract multiply divide
+
+function operate(calculationString) {
+  let parts = calculationString.split(/(\s*[-+*/]\s*)/);
+
+  parts = parts
+    .map((part) => part.trim())
+    .map((part) => (isNaN(part) ? part : parseInt(part)));
+
+  let [left, operator, right] = parts;
+
+  switch (operator) {
+    case '+':
+      return left + right;
+    case '-':
+      return left - right;
+
+    case '*':
+      return left * right;
+
+    case '/':
+      return left / right;
+    default:
+      return NaN;
+  }
+}
